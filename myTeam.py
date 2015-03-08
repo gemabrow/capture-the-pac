38c432a8c62c940bbd705ea37ea49e4fabc7d683
gemabrow <gemabrow@users.noreply.github.com>
2015-03-07 19:25:42 -0800

    Started implementation of qLearning Primary Agent

--- myTeam.py
+++ myTeam.py
@@ -8,7 +8,7 @@
 
 from captureAgents import CaptureAgent
 import random, time, util
-from game import Directions
+from game import Directions, Actions
 import game
 
 #################
@@ -391,6 +391,63 @@ class PrimaryAgent(BaseAgent):
   ***         game and descalates rapidly
   ***         learning rate = 1/(i**2), where i is index of games n, 1 <= i <= n
   """
+  def __init__(self, alpha=1.0, epsilon=0.05, gamma=0.8, numTraining = 10):
+    """
+    Sets options, which can be passed in via the Pacman command line using -a alpha=0.5,...
+    alpha    - learning rate
+    epsilon  - exploration rate
+    gamma    - discount factor
+    numTraining - number of training episodes, i.e. no learning after these many episodes
+    """
+    self.alpha = float(alpha)
+    self.epsilon = float(epsilon)
+    self.discount = float(gamma)
+    self.numTraining = int(numTraining)
+    self.index  # This is always the primary agent
+    self.featExtractor = 
+    # initialize the weights to be assigned to varying features
+    self.weights = util.Counter()
+  ####################################
+  #    Override These Functions      #
+  ####################################
+  def getQValue(self, state, action):
+    """
+    Should return Q(state,action)
+    """
+    util.raiseNotDefined()
+
+  def getValue(self, state):
+    """
+    What is the value of this state under the best action?
+    Concretely, this is given by
+
+    V(s) = max_{a in actions} Q(s,a)
+    """
+    util.raiseNotDefined()
+
+  def getPolicy(self, state):
+    """
+    What is the best action to take in the state. Note that because
+    we might want to explore, this might not coincide with getAction
+    Concretely, this is given by
+
+    policy(s) = arg_max_{a in actions} Q(s,a)
+
+    If many actions achieve the maximal Q-value,
+    it doesn't matter which is selected.
+    """
+    util.raiseNotDefined()
+
+  def getAction(self, state):
+    """
+    Simply calls the getAction method of QLearningAgent and then
+    informs parent of action for Pacman.  Do not change or remove this
+    method.
+    """
+    action = QLearningAgent.getAction(self,state)
+    self.doAction(state,action)
+    return action
+#******************************************************FEATURE EXTRACTOR BUSINESS********************************************************************************
   def getFeatures(self, gameState, action):
     features = util.Counter()
     successor = self.getSuccessor(gameState, action)
@@ -463,3 +520,78 @@ def setEnemyPositions(gameState, enemyPositions, enemyIndices):
     gameState.data.agentStates[enemyIndices[index + 1]] = game.AgentState(conf, False)
   return gameState  
 
+class FeatureExtractor:  
+  def getFeatures(self, state, action):    
+    """
+      Returns a dict from features to counts
+      Usually, the count will just be 1.0 for
+      indicator functions.  
+    """
+    util.raiseNotDefined()
+
+class IdentityExtractor(FeatureExtractor):
+  def getFeatures(self, state, action):
+    feats = util.Counter()
+    feats[(state,action)] = 1.0
+    return feats
+
+def closestFood(pos, food, walls):
+  """
+  closestFood -- this is similar to the function that we have
+  worked on in the search project; here its all in one place
+  """
+  fringe = [(pos[0], pos[1], 0)]
+  expanded = set()
+  while fringe:
+    pos_x, pos_y, dist = fringe.pop(0)
+    if (pos_x, pos_y) in expanded:
+      continue
+    expanded.add((pos_x, pos_y))
+    # if we find a food at this location then exit
+    if food[pos_x][pos_y]:
+      return dist
+    # otherwise spread out from the location to its neighbours
+    nbrs = Actions.getLegalNeighbors((pos_x, pos_y), walls)
+    for nbr_x, nbr_y in nbrs:
+      fringe.append((nbr_x, nbr_y, dist+1))
+  # no food found
+  return None
+
+class MasterExtractor:
+  """
+  Returns simple features for a basic reflex Pacman:
+  - whether food will be eaten
+  - how far away the next food is
+  - whether a ghost collision is imminent
+  - whether a ghost is one step away
+  """
+  
+  def getFeatures(self, state, action):
+    # extract the grid of food and wall locations and get the ghost locations
+    food = state.getFood()
+    walls = state.getWalls()
+    ghosts = state.getGhostPositions()
+
+    features = util.Counter()
+    
+    features["bias"] = 1.0
+    
+    # compute the location of pacman after he takes the action
+    x, y = state.getPacmanPosition()
+    dx, dy = Actions.directionToVector(action)
+    next_x, next_y = int(x + dx), int(y + dy)
+    
+    # count the number of ghosts 1-step away
+    features["#-of-ghosts-1-step-away"] = sum((next_x, next_y) in Actions.getLegalNeighbors(g, walls) for g in ghosts)
+
+    # if there is no danger of ghosts then add the food feature
+    if not features["#-of-ghosts-1-step-away"] and food[next_x][next_y]:
+      features["eats-food"] = 1.0
+    
+    dist = closestFood((next_x, next_y), food, walls)
+    if dist is not None:
+      # make the distance a number less than one otherwise the update
+      # will diverge wildly
+      features["closest-food"] = float(dist) / (walls.width * walls.height) 
+    features.divideAll(10.0)
+    return features
