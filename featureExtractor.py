0bf84c20acb93ffd87e0ba9805aeb860b057f962
gemabrow <gemabrow@users.noreply.github.com>
2015-03-12 23:10:12 -0700

    Still tweaking featureExtractor.py

--- featureExtractor.py
+++ featureExtractor.py
@@ -12,10 +12,10 @@ from game import Directions, Actions
 from layout import getLayout
 import util
 
-def closestFood(pos, food, walls):
+def closestInstance(pos, searchMatrix, walls):
   """
-  Finds food nearest a position, 
-  based on a passed in matrix of food, 
+  Finds instance nearest a position, based on 
+  a passed in matrix of boolean values, 
   and returns the maze distance to it
   """
   initialPos = pos
@@ -26,14 +26,14 @@ def closestFood(pos, food, walls):
     if (pos_x, pos_y) in expanded:
       continue
     expanded.add((pos_x, pos_y))
-    # if we find a food at this location then exit
-    if food[pos_x][pos_y]:
+    # if we find an instance at this location then exit
+    if searchMatrix[pos_x][pos_y]:
       return dist
     # otherwise spread out from the location to its neighbours
     nbrs = Actions.getLegalNeighbors((pos_x, pos_y), walls)
     for nbr_x, nbr_y in nbrs:
       fringe.append((nbr_x, nbr_y, dist+1))
-  # no food found
+  # no instance found
   return None
 
 class MasterExtractor:
@@ -59,50 +59,87 @@ class MasterExtractor:
   def __init__(self, myAgent):
     # passing in agent should give us access to beliefs
     self.agent = myAgent
-    self.distancer = Distancer( getLayout('defaultCapture') )
-    self.distancer.getMazeDistances()
+    #self.distancer = Distancer( getLayout('defaultCapture') )
+    #self.distancer.getMazeDistances()
 
   def getFeatures(self, gameState, action):
     features = util.Counter()
     features['score'] = self.agent.getScore(gameState)
     
-    # ***************** agent features ***********************
+    # ***************** features of agents ***********************
     initialPos = gameState.getInitialAgentPosition(self.agent.index)
     agentState = gameState.getAgentState(self.agent.index)
     scaredTime = agentState.scaredTimer
-    features['no-stop'] = -10 if action is Directions.STOP else 1
-    # Computes whether we're on the red or blue team
-    features['red'] = 1 if self.agent.red else -1
-    # Computes whether we're on defense (1) or offense (0)
-    features['onDefense'] = 1 if not agentState.isPacman else 0
-    # Computes whether our teammate is on defense (1) or offense (0)
-    features['friendOnD'] = 1 if not gameState.getAgentState(self.agent.friendIndex).isPacman else 0
-    friendPos = gameState.getAgentPosition(self.agent.friendIndex)
     # instead of generating successor, find next position given action
     # NOTE: assumes legal action given
+    if action not in gameState.getLegalActions(self.agent.index): 
+      print "WHAT ARE YOU DOING HERE, YOUNG MAN?"
     x, y = agentState.getPosition()
     dx, dy = Actions.directionToVector(action)
     next_x, next_y = int(x + dx), int(y + dy)
     myPos = next_x, next_y
+    features['no-stop'] = -10 if action is Directions.STOP else 1
+    # Computes whether we're on defense (1) or offense (0)
+    features['onDefense'] = 1 if not agentState.isPacman else 2
+    # Computes whether our teammate is on defense (1) or offense (0)
+    features['friendOnD'] = 2 if not gameState.getAgentState(self.agent.friendIndex).isPacman else 1
+    friendPos = gameState.getAgentPosition(self.agent.friendIndex)
+    # enemyPositions refers to the best guess from exact inference module
+    self.enemyPositions = self.agent.getEnemyPositions(gameState)
+    enemies = []
+    for enemyIndex in self.agent.enemyIndices:
+      enemy.index = enemyIndex
+      enemy.pos = self.enemyPositions[enemyIndex]
+      enemy.state = gameState.getAgentState(enemyIndex)
+      enemies.append(enemy)
+    
     # ************************* game features **************************************
     walls = gameState.getWalls()
-    features['available-moves'] = len(Actions.getLegalNeighbors(myPos, walls))
+    " LIKE A BAT OUTTA HELL "
+    features['initial-column'] = -666 if myPos[0] is initialPos[0] else 1337
+    " WHAT'RE MY OPTIONS, HMMMM??? "
+    features['available-moves-from-successor'] = len(Actions.getLegalNeighbors(myPos, walls))
     eatFood = self.agent.getFood(gameState)
     defendFood = self.agent.getFoodYouAreDefending(gameState)
-    try:
-      eatCapsules = util.matrixAsList(self.agent.getCapsules(gameState))
-      if len(eatCapsules) > 0:
-        minDistance = 1/(min([self.distancer.getDistance(myPos, capsule) for capsule in eatCapsules])+0.01)
-        features['distanceToEatCapsule'] = minDistance
-    except IndexError:
-      "Index error for capsules thrown"
-    self.enemyPositions = self.agent.getEnemyPositions(gameState)
-    enemy1, enemy2 = [self.enemyPositions[enemyIndex] for enemyIndex in self.agent.enemyIndices]
-    features['initial-column'] = -1000 if myPos[0] is initialPos[0] else 5
-    features['distance-to-enemy1'] = self.distancer.getDistance(myPos, enemy1)
-    features['distance-to-enemy2'] = self.distancer.getDistance(myPos, enemy2)
-    features['halfway-point-distance'] = abs(next_x - (gameState.getWalls().width / 2) )
+    "TIME TO PLAY SOME D"
+    if features['onDefense'] == 1 and scaredTime == 0:
+    # if we're a ghost and not under the effect of a power capsule
+      try:
+        defendCapsules = self.agent.getCapsulesYouAreDefending(gameState)
+        capsuleThreat = min(enemies, key = lambda enemy: closestInstance(enemy.pos, defendCapsules, walls))
+        enemyToCapsule = closestInstance(capsuleThreat, defendCapsules, walls)
+        features['threatened-capsule'] = float(5/enemyToCapsule)
+        features['distance-to-capsule-threat'] = float(4/self.agent.getDistance(myPos, capsuleThreat.pos))
+      except IndexError:
+        "No capsules to defend"
+      # tuple of enemy instance closest to food and positions - (distance, enemy position)
+      foodThreat = min(enemies, key = lambda enemy: closestInstance(enemy[0], defendFood, walls))
+      enemyToFood = closestInstance(foodThread, defendFood, walls)
+      features['threatened-food'] = float(2/enemyToFood)
+      features['distance-to-food-threat'] = float(3/self.agent.getDistance(myPos, foodThreat.pos))
+    elif features['onDefense'] == 1 and scaredTime > 0:
+    # do something for scared ghost
+      print 'aaaah! IMMA SCARED GHOST!'
+    else:
+    # offensive manuevers
+      try:
+        eatCapsules = util.matrixAsList(self.agent.getCapsules(gameState))
+        closestCapsule = min(eatCapsules, key = lambda capsule: self.agent.getDistance(myPos, capsule))
+        closestEnemy = min(enemies, key = lambda enemy: closestInstance(enemy.pos, myPos, walls))
+        enemyDistance = self.agent.getDistance(myPos, closestEnemy.pos)
+        enemyToCapsule = self.agent.getDistance(closestCapsule, closestEnemy.pos)
+        meToCapsule = self.agent.getDistance(myPos, closestCapsule)
+        if meToCapsule < enemyToCapsule:
+          features['capsule-craving'] = float(5/enemyDistance*meToCapsule)
+      except IndexError:
+        "No capsules to eat"
+        
+    # trend towards middle
+    features['halfway-point-distance'] = 1 / (  abs( next_x - (gameState.getWalls().width / 2) )  )
+    # and away from the column of initial spawning
     features['distance-from-home'] = 10 * abs(initialPos[0] - myPos[0])
+    # and trend away from teammate
+    features['spread-and-destroy'] = 2 * self.distancer.getDistance(myPos, friendPos)
     # Compute distance to the nearest food
     foodList = eatFood.asList()
     if len(foodList) > 0: # This should always be True,  but better safe than sorry
